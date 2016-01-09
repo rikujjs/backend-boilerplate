3f832b8cd2a9a4ffa0d9f8439fd3994deb4f50d9
Riku Marttila <riku.marttila@vincit.fi>
2016-01-09 15:18:39 +0200

    Apply all files found in the /routes dir

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const Knex = require('knex');
 const knexConfig = require('./knexfile');
-const userRoute = require('./routes/user');
 const Model = require('objection').Model;
+const fs = require('fs');
 
 const app = express()
   .use(bodyParser.json())
@@ -16,8 +16,11 @@ const app = express()
 const knex = Knex(knexConfig.development);
 Model.knex(knex);
 
-//TODO: loop all files in route dir and apply
-userRoute(app);
+const routeFiles = fs.readdirSync(__dirname + '/routes');
+routeFiles.forEach((file) => {
+  let routeFile = require('./routes/' + file);
+  routeFile(app);
+});
 
 const server = app.listen(3000, function() {
   console.log("Server running! port: %s!", server.address().port);
